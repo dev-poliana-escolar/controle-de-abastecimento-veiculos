2152b9aeacc73953aa45ddcf86d02be706d93f95
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-11-11 21:38:25 -0300

    feat: base da aplicação importei modulos importantes

--- controle-abastecimento/src/app/app.ts
+++ controle-abastecimento/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('controle-abastecimento');
+  valor_litro : number = 0;
+  qtd_abastecido : number = 0;
 }
